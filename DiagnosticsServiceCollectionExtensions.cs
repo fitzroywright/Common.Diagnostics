55cf253b3938e8a0a3a57c69227567c4200c694f
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-08 07:55:56 -0500

    Add diagnostics dependency injection extensions

---
+++ DiagnosticsServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+namespace Common.Diagnostics;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DiagnosticsServiceCollectionExtensions
+{
+    public static IServiceCollection AddCommonDiagnostics(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        services.AddScoped<IDiagnosticRunner, DiagnosticRunner>();
+        return services;
+    }
+
+    public static IServiceCollection AddDiagnosticCheck<TCheck>(this IServiceCollection services)
+        where TCheck : class, IDiagnosticCheck
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        services.AddScoped<IDiagnosticCheck, TCheck>();
+        return services;
+    }
+}
