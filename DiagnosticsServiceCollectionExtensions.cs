4e515852e2224676ff23e391163cfc2c7dbe803f
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-09 10:49:51 -0500

    Register standard diagnostic telemetry transports

--- DiagnosticsServiceCollectionExtensions.cs
+++ DiagnosticsServiceCollectionExtensions.cs
@@ -22,6 +22,30 @@ public static class DiagnosticsServiceCollectionExtensions
         return services;
     }
 
+    public static IServiceCollection AddStandardDiagnosticTelemetry(
+        this IServiceCollection services,
+        GraylogTelemetryOptions? graylog = null,
+        WazuhTelemetryOptions? wazuh = null,
+        SlackTelemetryOptions? slack = null,
+        EmailTelemetryOptions? email = null)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddSingleton(graylog ?? new GraylogTelemetryOptions());
+        services.AddSingleton(wazuh ?? new WazuhTelemetryOptions());
+        services.AddSingleton(slack ?? new SlackTelemetryOptions());
+        services.AddSingleton(email ?? new EmailTelemetryOptions());
+        services.AddSingleton<GraylogDiagnosticTelemetryDestination>();
+        services.AddSingleton<IDiagnosticTelemetryDestination>(provider => provider.GetRequiredService<GraylogDiagnosticTelemetryDestination>());
+        services.AddHttpClient<WazuhDiagnosticTelemetryDestination>();
+        services.AddSingleton<IDiagnosticTelemetryDestination>(provider => provider.GetRequiredService<WazuhDiagnosticTelemetryDestination>());
+        services.AddHttpClient<SlackDiagnosticTelemetryDestination>();
+        services.AddSingleton<IDiagnosticTelemetryDestination>(provider => provider.GetRequiredService<SlackDiagnosticTelemetryDestination>());
+        services.AddSingleton<EmailDiagnosticTelemetryDestination>();
+        services.AddSingleton<IDiagnosticTelemetryDestination>(provider => provider.GetRequiredService<EmailDiagnosticTelemetryDestination>());
+        return services;
+    }
+
     public static IServiceCollection AddDiagnosticCheck<TCheck>(this IServiceCollection services)
         where TCheck : class, IDiagnosticCheck
     {
