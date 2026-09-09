70838687e7a0fce2b847f139067aafdd81565d30
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-09 09:44:41 -0500

    Register centralized telemetry operations services

--- DiagnosticsServiceCollectionExtensions.cs
+++ DiagnosticsServiceCollectionExtensions.cs
@@ -1,15 +1,23 @@
 namespace Common.Diagnostics;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 public static class DiagnosticsServiceCollectionExtensions
 {
     public static IServiceCollection AddCommonDiagnostics(this IServiceCollection services)
     {
         ArgumentNullException.ThrowIfNull(services);
+
         services.AddScoped<IDiagnosticRunner, DiagnosticRunner>();
         services.AddSingleton<IEngineeringDiagnosticRunStore, JsonEngineeringDiagnosticRunStore>();
         services.AddSingleton<EngineeringDiagnosticEngine>();
+
+        services.TryAddSingleton(new DiagnosticOperationsOptions());
+        services.TryAddSingleton<IDiagnosticSuppressionStore, InMemoryDiagnosticSuppressionStore>();
+        services.TryAddSingleton<IDiagnosticRetentionStore, JsonDiagnosticRetentionStore>();
+        services.TryAddSingleton<IDiagnosticOperationsRouter, DiagnosticOperationsRouter>();
+
         return services;
     }
 
@@ -18,6 +26,15 @@ public static class DiagnosticsServiceCollectionExtensions
     {
         ArgumentNullException.ThrowIfNull(services);
         services.AddScoped<IDiagnosticCheck, TCheck>();
+        return services;
+    }
+
+    public static IServiceCollection AddDiagnosticTelemetryDestination<TDestination>(
+        this IServiceCollection services)
+        where TDestination : class, IDiagnosticTelemetryDestination
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        services.AddSingleton<IDiagnosticTelemetryDestination, TDestination>();
         return services;
     }
 }
