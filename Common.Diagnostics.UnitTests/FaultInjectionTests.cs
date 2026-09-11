109f917f78a10b2fafb4e25fc40723615e45e73e
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-11 08:49:09 -0500

    Use compatible xUnit string assertion

--- Common.Diagnostics.UnitTests/FaultInjectionTests.cs
+++ Common.Diagnostics.UnitTests/FaultInjectionTests.cs
@@ -43,7 +43,7 @@ public sealed class FaultInjectionTests
         DiagnosticResult result = await check.RunAsync();
 
         Assert.Equal(DiagnosticStatus.Unhealthy, result.Status);
-        Assert.Contains("503", result.Message, StringComparison.Ordinal);
+        Assert.Contains("503", result.Message);
     }
 
     [Fact]
